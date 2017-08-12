88843947ce0ab836e45c2a42d8cde6182812c997
Frederic Poulin <fredpoulin@gmail.com>
2017-08-12 08:30:32 -0400

    Fixing merge issues

--- lisp/init-flycheck.el
+++ lisp/init-flycheck.el
@@ -3,7 +3,7 @@
   (setq flycheck-display-errors-function #'flycheck-display-error-messages-unless-error-list)
 
   (when (maybe-require-package 'flycheck-color-mode-line)
-    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode)))
+    (add-hook 'flycheck-mode-hook 'flycheck-color-mode-line-mode))
 
   (after-load 'guide-key
     (add-to-list 'guide-key/guide-key-sequence "C-c !")))
--- lisp/init-themes.el
+++ lisp/init-themes.el
@@ -3,7 +3,7 @@
 (require-package 'hc-zenburn-theme)
 
 ;; If you don't customize it, this is the theme you get.
-(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
+;(setq-default custom-enabled-themes '(sanityinc-tomorrow-bright))
 (setq-default custom-enabled-themes '(hc-zenburn))
 
 ;; Ensure that themes will be applied even if they have not been customized
